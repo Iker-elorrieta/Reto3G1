ab07175e66a0df0f75a9ad0f0f9a0a25f11eb033
in1dm3 <in1dm3@HZ352020.elorrieta.int>
2023-01-30 09:12:18 +0100

    Commit Obje

--- Reto3/src/modelo/Cliente.java
+++ Reto3/src/modelo/Cliente.java
@@ -1,5 +1,66 @@
 package modelo;
 
+import java.util.Objects;
+
 public class Cliente {
+	String dni;
+	String nombre;
+	String apellido_1;
+	String apellido_2;
+	char  sexo;
+	String contraseña;
+	
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido_1() {
+		return apellido_1;
+	}
+	public void setApellido_1(String apellido_1) {
+		this.apellido_1 = apellido_1;
+	}
+	public String getApellido_2() {
+		return apellido_2;
+	}
+	public void setApellido_2(String apellido_2) {
+		this.apellido_2 = apellido_2;
+	}
+	public char getSexo() {
+		return sexo;
+	}
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(apellido_1, apellido_2, contraseña, dni, nombre, sexo);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(dni, other.dni);
+	}
 
 }
--- Reto3/src/modelo/Pelicula.java
+++ Reto3/src/modelo/Pelicula.java
@@ -1,7 +1,45 @@
 package modelo;
 
+import java.util.Objects;
+
 public class Pelicula {
 	float precio;
+	String cdPel;
+	String nombre;
+	float duración;
+	String género; 
+	
+	public String getCdPel() {
+		return cdPel;
+	}
+
+	public void setCdPel(String cdPel) {
+		this.cdPel = cdPel;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public float getDuración() {
+		return duración;
+	}
+
+	public void setDuración(float duración) {
+		this.duración = duración;
+	}
+
+	public String getGénero() {
+		return género;
+	}
+
+	public void setGénero(String género) {
+		this.género = género;
+	}
 
 	public float getPrecio() {
 		return precio;
@@ -9,6 +47,24 @@ public class Pelicula {
 
 	public void setPrecio(float precio) {
 		this.precio = precio;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(cdPel, duración, género, nombre, precio);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pelicula other = (Pelicula) obj;
+		return Objects.equals(cdPel, other.cdPel);
 	}
 	
 
