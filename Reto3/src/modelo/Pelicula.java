533b2d73249db6b5027ced7ba2e7a38abdae4a7c
in1dm3 <in1dm3@HZ352020.elorrieta.int>
2023-01-30 09:11:32 +0100

    Commit Objetos

---
+++ Reto3/src/modelo/Pelicula.java
@@ -0,0 +1,15 @@
+package modelo;
+
+public class Pelicula {
+	float precio;
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	
+
+}
