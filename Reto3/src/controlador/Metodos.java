b9f7f3c6c72fc12ae5bc237c5a755c67b54a49ed
in1dm3 <in1dm3@HZ352021.elorrieta.int>
2023-02-01 12:58:37 +0100

    commit 14

--- Reto3/src/controlador/Metodos.java
+++ Reto3/src/controlador/Metodos.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 import modelo.Cine;
 
 public class Metodos {
-	
+	// declaro la base de datos remota junto al usuario y contraseña de mysql que he creado allí.
 	final String sConexion="jdbc:mysql://10.5.14.202:3306/cines";
 	final String user = "cliente";
 	final String contra="Contraseña33#";
@@ -17,14 +17,13 @@ public class Metodos {
 	
 	
 	
-
+	// Aqui lee los datos de la tabla cines de la BD y la mete en una array que devuelve
 	public Cine[] cuantosCines() {
 		// TODO Auto-generated method stub
 		Cine[] cines= new Cine[0];
 		
 		
 		try {
-			//DriverManager.registerDriver(new com.mysql.jdbc.Driver()); 
 			Connection conexion = DriverManager.getConnection(sConexion, user, contra);
 			Statement comando = conexion.createStatement();
 			ResultSet registro = comando.executeQuery("SELECT * FROM cines;");
