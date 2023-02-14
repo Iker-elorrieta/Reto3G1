9637387483a081fad198f928d6604dcfd7135e88
in1dm3 <in1dm3@HZ352021.elorrieta.int>
2023-02-14 12:58:44 +0100

    commit de vista ticket

--- Reto3/src/vista/VistaTicket.java
+++ Reto3/src/vista/VistaTicket.java
@@ -21,7 +21,7 @@ public class VistaTicket extends JFrame {
 	 */
 	public VistaTicket(Entrada[] entradas, String dni) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 589, 459);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
